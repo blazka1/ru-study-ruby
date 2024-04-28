7258c6333103178e9041c5c51610105c5336bc44
Stanislav_Patratiy <blazka2011@yandex.ru>
2024-04-28 15:32:51 +0300

    add functions rating, chars_count in exercise/fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,28 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        valid_films = _array.select do |film|
+          countries = film['country'].split(',') if film['country']
+          film['rating_kinopoisk'].to_f.positive? && countries && countries.size > 1
+        end
+
+        ratings = valid_films.map { |film| film['rating_kinopoisk'].to_f }
+
+        if ratings.size.positive?
+          ratings.reduce(0.0) { |sum, rating| sum + rating } / ratings.size
+        else
+          0
+        end
       end
 
       def chars_count(_films, _threshold)
-        0
+        kinopoisk_list = _films.select do |film|
+          film['rating_kinopoisk'].to_f >= _threshold
+        end
+
+        kinopoisk_list.reduce(0) do |sum, film|
+          sum + film['name'].count('и')
+        end
       end
     end
   end
