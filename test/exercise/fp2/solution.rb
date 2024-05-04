44911608d81b1c2fa7a37c459cb7b925795fdfee
Stanislav Patratiy <blazka2011@yandex.ru>
2024-05-04 23:18:20 +0300

    add functions my_map, my_compact, my_reduce in exercise/fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -7,22 +7,39 @@ module Exercise
       # Написать свою функцию my_each
       def my_each
         index = 0
-        while index < self.length
+        while index < length
           yield self[index]
           index += 1
         end
         self
       end
-      
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+        my_each do |element|
+          result << yield(element)
+        end
+        result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+        my_each do |element|
+          result << element unless element.nil?
+        end
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial = nil)
+        acc = initial
+        my_each do |element|
+          acc = acc.nil? ? element : yield(acc, element)
+        end
+        acc
+      end
     end
   end
 end
