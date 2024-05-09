5f61ca7e59bef3ba9bcf162b5ac4c411fbc45c0d
Stanislav Patratiy <blazka2011@yandex.ru>
2024-05-10 01:24:16 +0300

    fix map, reduce. compact, each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,44 +1,53 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      # Использовать стандартные функции массива для решения задач нельзя.
-      # Использовать свои написанные функции для реализации следующих - можно.
-
-      # Написать свою функцию my_each
       def my_each
-        index = 0
-        while index < length
+        recurse = lambda do |index|
+          return if index >= size
+
           yield self[index]
-          index += 1
+          recurse.call(index + 1)
         end
+        recurse.call(0)
         self
       end
 
-      # Написать свою функцию my_map
       def my_map
-        result = MyArray.new
-        my_each do |element|
-          result << yield(element)
+        mapper = lambda do |result, index|
+          return result if index >= size
+
+          result << yield(self[index])
+
+          mapper.call(result, index + 1)
         end
-        result
+
+        mapper.call(MyArray.new, 0)
       end
 
-      # Написать свою функцию my_compact
       def my_compact
-        result = MyArray.new
-        my_each do |element|
-          result << element unless element.nil?
+        compact_recursively = lambda do |index, accum|
+          return MyArray.new(accum) if index == size
+
+          current_element = self[index]
+          next_accum = current_element.nil? ? accum : accum + [current_element]
+
+          compact_recursively.call(index + 1, next_accum)
         end
-        result
+
+        compact_recursively.call(0, [])
       end
 
-      # Написать свою функцию my_reduce
-      def my_reduce(initial = nil)
-        acc = initial
-        my_each do |element|
-          acc = acc.nil? ? element : yield(acc, element)
+      def my_reduce(acc = nil, index = 0, &block)
+        raise ArgumentError, 'no block given' unless block_given?
+
+        if acc.nil? && !empty?
+          acc = self[0]
+          index = 1
         end
-        acc
+
+        return acc if index >= size
+
+        my_reduce(yield(acc, self[index]), index + 1, &block)
       end
     end
   end
